e2387fb22de5c2d1cc70920da3c1f11d11c5d119
areumh01 <areumh01@gmail.com>
2023-04-25 21:37:41 +0900

    필드 수정/추가

--- src/main/java/com/noldaga/domain/FeedDto.java
+++ src/main/java/com/noldaga/domain/FeedDto.java
@@ -4,32 +4,34 @@ import com.noldaga.domain.entity.Feed;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
-@AllArgsConstructor
+@Setter
 @Getter //response 로 변환해줄때 필요 등
 public class FeedDto {
     private Long id;
     private String title;
     private String content;
     private UserDto userDto;
-    private long groupId;
+    private Long groupId;
     private int range;
     private LocalDateTime modDate;
     private LocalDateTime regDate;
-    private long totalView;
+    private Long totalView;
 
-    /*
-    private FeedDto(Long id, String title, String content, UserDto userDto, LocalDateTime createdAt, LocalDateTime modifiedAt, LocalDateTime deletedAt) {
+    private FeedDto(Long id, String title, String content, UserDto userDto, Long groupId, int range, LocalDateTime modDate, LocalDateTime regDate, Long totalView) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.userDto = userDto;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-        this.deletedAt = deletedAt;
+        this.groupId = groupId;
+        this.range = range;
+        this.modDate = modDate;
+        this.regDate = regDate;
+        this.totalView = totalView;
     }
 
     public static FeedDto fromEntity(Feed feed) {
@@ -38,10 +40,12 @@ public class FeedDto {
                 feed.getTitle(),
                 feed.getContent(),
                 UserDto.fromEntity(feed.getUser()), //user 엔티티가 아니라 userDto 이어야함
-                feed.getCreatedAt(),
-                feed.getModifiedAt(),
-                feed.getDeletedAt()
+                feed.getGroupId(),
+                feed.getRange(),
+                feed.getModDate(),
+                feed.getRegDate(),
+                feed.getTotalView()
         );
     }
-     */
+
 }
