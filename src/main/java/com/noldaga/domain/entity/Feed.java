443e423fc9f19a6b62272ab48d36e66d8c0a7d06
areumh01 <areumh01@gmail.com>
2023-04-25 15:49:25 +0900

    Feed Entity수정

--- src/main/java/com/noldaga/domain/entity/Feed.java
+++ src/main/java/com/noldaga/domain/entity/Feed.java
@@ -1,9 +1,7 @@
 package com.noldaga.domain.entity;
 
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
@@ -12,15 +10,17 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @Getter //Dto 만들때 쓰임
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Table(name="feed") //db 테이블 만들때 예약어는 피해야하는것을 염두해야함
 @Entity
-@SQLDelete(sql = "update feed set deleted_at = now() where feed_id=? ") //소프트 delete를 위함...
-@Where(clause = "deleted_at is NULL") // 조회시 이 조건이 자동 추가
 public class Feed {
 
     @Id
@@ -29,11 +29,12 @@ public class Feed {
     private Long id;
 
     @Setter
-    @Column(nullable=false )
+    @NotEmpty
+    @Column(nullable = false, length = 100, name="title")
     private String title;
 
+    @NotEmpty
     @Setter
-//    @Column(nullable = false,length = 1000)
     @Column(nullable=false, columnDefinition = "TEXT")//TEXT 타입으로 컬럼이 생성됨(그냥 String 디폴트보다 더 길게 저장가능)
     private String content;
 
@@ -41,30 +42,36 @@ public class Feed {
     @JoinColumn(name="user_id")
     private User user;
 
+//    Group_id는 selectbox에서 선택하는 값으로 group을 저장하려면 다시 조회해서 들고와야해서 일단 id만 저장
+//    @OneToOne
+//    @JoinColumn(name="group_id")
+//    private Group group;
+    @Setter
+    @Column(nullable = false, name="group_id")
+    private long groupId;
+
+    @Setter
+    @Column(nullable = false, name="open_range") //전체 0 부분공개 1
+    private int range;
+
     @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
-    @Column(nullable = false , updatable = false)
-    private LocalDateTime createdAt;
+    @Column(nullable = false , updatable = false, name="reg_date")
+    private LocalDateTime regDate;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
+    @Column(nullable = false, name="mod_date")
+    private LocalDateTime modDate;
 
-    @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime deletedAt;
+    @Column(name="total_view")
+    private long totalView;
 
-    protected Feed(){
+    public void change(String title, String content, long groupId, int range){
+        this.setTitle(title);
+        this.setContent(content);
+        this.setGroupId(groupId);
+        this.setRange(range);
     }
 
-    public Feed(Long feedId,String title, String content,User user) {
-        this.id = feedId;
-        this.title = title;
-        this.content = content;
-        this.user = user;
-    }
-
-    public static Feed of(String title, String content,User user) {
-        return new Feed(null,title, content,user);
-    }
 }
