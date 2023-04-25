fcbf4a996514f98a70f1b91885e765088f7f7e40
deliciousdev <deliciousdev@naver.com>
2023-04-25 14:33:39 +0900

    init

---
+++ src/main/java/com/noldaga/NoldagaApplication.java
@@ -0,0 +1,13 @@
+package com.noldaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoldagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoldagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noldaga/NoldagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noldaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoldagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
