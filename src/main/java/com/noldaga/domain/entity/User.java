92b5ebeaa21627699bd77671369798185dab73ce
deliciousdev <deliciousdev@naver.com>
2023-04-27 17:17:32 +0900

    user,userDto 필드에 nickname추가

--- src/main/java/com/noldaga/domain/entity/User.java
+++ src/main/java/com/noldaga/domain/entity/User.java
@@ -32,6 +32,7 @@ public class User {
     @Column(nullable=false ,updatable = false,unique = true)
     private String username;
 
+    private String nickname;
     @Setter @Column(nullable = false)
     private String password;
 
