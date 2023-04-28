644a08e74083114c728649af207449d0f7f0d967
deliciousdev <deliciousdev@naver.com>
2023-04-28 22:54:39 +0900

    이메일 보내는 api , 임시코드체크 api 컨트롤러에 추가

--- src/main/java/com/noldaga/controller/UserController.java
+++ src/main/java/com/noldaga/controller/UserController.java
@@ -1,17 +1,23 @@
 package com.noldaga.controller;
 
 
-import com.noldaga.controller.request.UserJoinRequest;
-import com.noldaga.controller.request.UserLoginRequest;
-import com.noldaga.controller.request.UsernameRequest;
+import com.noldaga.controller.request.*;
+import com.noldaga.controller.response.CodeIdResponse;
 import com.noldaga.controller.response.Response;
 import com.noldaga.controller.response.UserJoinResponse;
+import com.noldaga.domain.CodeDto;
 import com.noldaga.domain.userdto.UserDto;
 import com.noldaga.exception.ErrorCode;
 import com.noldaga.exception.SnsApplicationException;
+import com.noldaga.module.CodeValidator;
+import com.noldaga.service.MailSendService;
 import com.noldaga.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,9 +26,13 @@ public class UserController {
 
     private final UserService userService;
 
+    private final MailSendService mailSendService;
+
+    private final CodeValidator codeValidator;
+
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest req) {
-        UserDto userDto = userService.join(req.getUsername(), req.getPassword(),req.getNickname(),req.getEmail());
+        UserDto userDto = userService.join(req.getUsername(), req.getPassword(), req.getNickname(), req.getEmail());
         UserJoinResponse userJoinResponse = UserJoinResponse.fromUserDto(userDto);
 
         return Response.success(userJoinResponse); //프론트에서 api의 반환값을 파싱하기 쉽도록 실패하든 성공하든 획일화된 포맷팅으로 보낸다.
@@ -35,8 +45,8 @@ public class UserController {
         return Response.success(token);
     }
 
-    @PostMapping("/check")
-    public Response<Void> login(@RequestBody UsernameRequest request){
+    @PostMapping("/check-duplicated")
+    public Response<Void> login(@RequestBody UsernameRequest request) {
         userService.check(request.getUsername()).ifPresent(it -> {
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USERNAME, String.format("%s is duplicated", it.getUsername()));
         });
@@ -44,6 +54,22 @@ public class UserController {
         return Response.success();
     }
 
+    @PostMapping("/email")
+    public Response<CodeIdResponse> email(@RequestBody MailAuthRequest req) throws MessagingException, UnsupportedEncodingException {
+
+        CodeDto codeDto= codeValidator.generateCode();
+        mailSendService.sendEmail(req.getEmailAddress(), codeDto.getCode());
+
+        return Response.success(CodeIdResponse.fromCodeDto(codeDto));
+    }
+
+    @PostMapping("/check-code")
+    public Response<Void> checkCode(@RequestBody CodeRequest req, HttpServletRequest httpRequest) {
+
+        codeValidator.validateCode(req.getCodeId(), req.getCode());
+
+        return Response.success();
+    }
 
 
 }
