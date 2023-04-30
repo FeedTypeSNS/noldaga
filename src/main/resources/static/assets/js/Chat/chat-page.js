806a0de4eab9c10d68a9d6227dc63f08c5fa7d20
Majiwoo <majiou4420@naver.com>
2023-04-30 21:23:08 +0900

    chat basic view update

--- src/main/resources/static/assets/js/Chat/chat-page.js
+++ src/main/resources/static/assets/js/Chat/chat-page.js
@@ -1,7 +1,7 @@
-fetch("/chat")
-        .then((res) =>res.json())
-        .then(()=>{
-            localStorage.setItem("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IuyYgeyImCIsImlhdCI6MTY4MjQ5NTI1MSwiZXhwIjoxNjg1MDg3MjUxfQ.3gEiYXQKKSoT8FWBTDQIPJ7aVqJK4ZczpwW4wfBMGLg");
-            alert("채팅 테스트");
-            this.props.history.push("/Chat");
-        })
+function init(){
+    $.ajax({
+        type:"GET",
+        utl:"",
+        dataType: ""
+    })
+}
---
+++ src/main/resources/static/assets/js/Chat/chat-side.js
@@ -0,0 +1,61 @@
+let side = {
+    init:function (){
+        $("#myFollowList").on("click",()=>{
+           this.getMyFollowList();
+        });
+        $("#myFollowList2").on("click",()=>{
+            this.getMyFollowList();
+        }); //팔로잉 리스트 반환
+
+    },
+
+    getMyFollowList:function (){
+        //const queryString = window.location.search;
+        $.ajax({
+            type: "GET",
+            url:"/api/following/my",
+            contentType:"application/json; charset=utf-8"
+        }).done(function (resp){
+            alert(JSON.stringify(resp));
+            followlist(resp);
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    }
+
+}
+
+function followlist(data){
+    $(".followlist *").remove(); //기존 내용 삭제 후 append
+    for(let i=0; i<data.result.length; i++) {
+        let followlistBox = document.querySelector('#sendCheckList');
+        let databox = document.createElement("div");
+        databox.className = "followlist";
+        databox.innerHTML = getFollowlistContent(data.result[i]);
+        followlistBox.append(databox);
+    }
+}
+function getFollowlistContent(data) {
+    return `                <li  style="list-style: none; padding-left: 0px;">
+                                    <div class="d-flex">
+                                        <div class="avatar">
+                                            <img class="avatar-img rounded-circle" src="assets/images/avatar/11.jpg" alt=""> <!--사용자 프로필이미지 열기-->
+                                        </div>
+                                        <div class="flex-grow-1 d-block">
+                                            <input style="margin: 0.4rem;zoom: 1.5; float: right" id="sendUserCheck" type="checkbox" name="joinUserList" value="${data.username}"/>
+
+                                            <h6 style="padding-left: 5px" class="mb-0 mt-1">${data.id} 번째  ${data.username} 회원</h6>
+                                            <div class="small text-secondary">&nbsp;회원 닉네임 추가</div>
+                                            <br>
+                                        </div>
+                                    </div>
+                            </li>
+
+`
+
+    /*`<li style="list-style: none; padding-left: 0px">${data.id} 번째  ${data.username} 회원
+    <input style="margin: 0.4rem;" id="sendUserCheck" type="checkbox" name="joinUserList" value="${data.username}"/></li>`;*/
+
+}
+
+side.init(); //초기화
