92f2d1eabfa0910a6fcbe7c34bc011b49865e554
Majiwoo <majiou4420@naver.com>
2023-04-30 21:26:47 +0900

    Basic Chat related Request & Response

---
+++ src/main/java/com/noldaga/controller/request/ChatSendRequest.java
@@ -0,0 +1,12 @@
+package com.noldaga.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor//test 에서 사용
+@Getter //serialize 에 사용
+@NoArgsConstructor //@RequestBody 매커니즘 의 Json 데이터 -> 자바객체   과정에서 사용됨 by jackson
+public class ChatSendRequest {
+    private String msg;
+}
