ed9c2197273919163902f49c700ba93ba6e8aad5
deliciousdev <deliciousdev@naver.com>
2023-05-01 18:56:06 +0900

    기존에 프론트에서 tokenCookie 작업하던것을 서버 UserService.login() 에서 작업하도록함 및 사소한 이름 변경

--- src/main/java/com/noldaga/service/UserService.java
+++ src/main/java/com/noldaga/service/UserService.java
@@ -13,6 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -47,7 +51,7 @@ public class UserService {
     }
 
     //토큰 반환
-    public String login(String username, String password) {
+    public String login(String username, String password, HttpServletResponse httpServletResponse) {
         //회원으로 등록되어있는 username 인지 체크
         User user = userRepository.findByUsername(username).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
@@ -60,6 +64,7 @@ public class UserService {
 
         //토큰생성
         String token = JwtTokenUtils.generateToken(username, key, expiredTimeMs);
+        generateTokenCookie(httpServletResponse,token);
 
         return token;
     }
@@ -71,33 +76,43 @@ public class UserService {
     }
 
 
-    public UserDto findUsernameByEmail(String emailAddress) {
+    public UserDto searchUsernameByEmail(String emailAddress) {
         return userRepository.findByEmail(emailAddress).map(UserDto::fromEntity).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s is invalid E-mailAddress", emailAddress)));
     }
 
-    public String findEmail(String username) {
+    public String searchEmail(String username) {
         UserDto userDto = userRepository.findByUsername(username).map(UserDto::fromEntity).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
         return userDto.getEmail();
     }
 
+    @Transactional
     public String initPassword(String username) {
         User user = userRepository.findByUsername(username).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
 
         String newPassword = codeGenerator.generateRandomCode(INIT_PASSWORD_SIZE);
         user.changePassword(encoder.encode(newPassword));
+        userRepository.save(user);//@Transactional 있으면 자동으로 save 되긴함
 
         return newPassword;
     }
 
+    //OAuth2UserService 에서 사용되는 메서드
+    public Optional<UserDto> loadUserByUsername(String username){
+        return userRepository.findByUsername(username).map(UserDto::fromEntity);
+    }
 
-//    public UserDto loadUserByUsername(String username) {
-//        return userRepository.findByUsername(username).map(UserDto::fromEntity).orElseThrow(() ->
-//                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username)));
-//    }
 
-
+    private void generateTokenCookie(HttpServletResponse response, String token) {
+        final int A_MONTH = 60 * 60 * 24 * 30;
+        Cookie tokenCookie = new Cookie("tokenCookie", token);
+        tokenCookie.setMaxAge(A_MONTH);
+        response.addCookie(tokenCookie);
+        tokenCookie.setHttpOnly(true); //자바스크립트로 조작 금지
+        tokenCookie.setPath("/");
+        response.addCookie(tokenCookie);
+    }
 
 }
