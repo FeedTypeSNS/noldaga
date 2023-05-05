49599c2ed412c86fff7fd0a2e1dc192090ccd714
deliciousdev <deliciousdev@naver.com>
2023-05-05 19:24:44 +0900

    파일 업로드 사이즈 너무 클때, @Validated 예외처리

--- src/main/java/com/noldaga/exception/GlobalControllerAdvice.java
+++ src/main/java/com/noldaga/exception/GlobalControllerAdvice.java
@@ -5,8 +5,10 @@ import com.noldaga.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 
 @Slf4j
@@ -17,16 +19,32 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(SnsApplicationException.class)
     public ResponseEntity<?> applicationHandler(SnsApplicationException e) {
         e.printStackTrace();
-        log.error("Error occurs {}",e.toString());
+        log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(e.getErrorCode().name()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> applicationHandler(RuntimeException e){
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
         e.printStackTrace();
-        log.error("Error occurs {}",e.toString());
+        log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.name()));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity applicationHandler(MaxUploadSizeExceededException e) {
+        e.printStackTrace();
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(Response.error(ErrorCode.UPLOAD_SIZE_TOO_LARGE.name()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity applicationHandler(MethodArgumentNotValidException e) {
+        e.printStackTrace();
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Response.error(ErrorCode.INVALID_DATA_VALUE.name()));
     }
 }
