e5550cd37f86c5492a91c9eb737ee5d4d72adc0d
deliciousdev <deliciousdev@naver.com>
2023-05-05 19:32:27 +0900

    데이터검증 어노테이션 추가

--- src/main/java/com/noldaga/controller/request/UserJoinRequest.java
+++ src/main/java/com/noldaga/controller/request/UserJoinRequest.java
@@ -4,12 +4,31 @@ package com.noldaga.controller.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor //test 코드에서 사용
 @Getter // @PostMapping 에서 json -> 자바객체로 바인딩 될때 getter 없어도 됨,  All생성자 or No생성자 둘중 하나 있으면됨 (ObjectMapper기반)
 public class UserJoinRequest {
 
+    @NotNull
+    @NotBlank
     private String username;
+
+    @NotNull
+    @NotBlank
     private String password;
+
+    @NotNull
+    @NotBlank
     private String nickname;
+
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+
+    private Integer codeId;
+    private String code;
 }
--- src/main/java/com/noldaga/controller/request/UsernameRequest.java
+++ src/main/java/com/noldaga/controller/request/UsernameRequest.java
@@ -4,8 +4,13 @@ package com.noldaga.controller.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @Getter // @PostMapping 에서 json -> 자바객체로 바인딩 될때 getter 없어도 됨,  All생성자 or No생성자 둘중 하나 있으면됨 (ObjectMapper기반)
 public class UsernameRequest {
+    @NotNull
+    @NotBlank
     private String username;
 }
