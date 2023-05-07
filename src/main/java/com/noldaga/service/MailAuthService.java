371fecd494c4d8f4b3fc1ed09b086a68876f6792
deliciousdev <deliciousdev@naver.com>
2023-05-07 18:20:31 +0900

    code_email 로 데이터 구조를 바꿈. 이메일을 변경할때도 비밀번호를 확인함.

--- src/main/java/com/noldaga/service/MailAuthService.java
+++ src/main/java/com/noldaga/service/MailAuthService.java
@@ -18,13 +18,29 @@ public class MailAuthService {
     private final MailSender mailSender;
     private final CodeValidator codeValidator;
 
-    public Integer sendCode(String emailAddress) throws MessagingException, UnsupportedEncodingException {
 
-        CodeDto codeDto = codeValidator.generateCode(emailAddress);
+
+    public Integer sendCodeForJoin(String emailAddress) throws MessagingException, UnsupportedEncodingException {
+
+        CodeDto codeDto = codeValidator.generateCodeForJoin(emailAddress);
         String message = "인증코드를 입력해주세요 " + codeDto.getCode();
         mailSender.sendEmail(emailAddress,message);
         return codeDto.getCodeId();
     }
+
+    public void validateCodeForJoin(Integer codeId, String code) {
+        codeValidator.validateCodeForJoin(codeId, code);
+
+    }
+
+
+    public void validateAuthenticatedEmail(Integer codeId,String email){
+//        codeValidator.validateAuthenticatedEmail(codeId,email);
+    }
+
+
+
+
 
     public Integer sendCodeForPassword(String emailAddress, String username) throws MessagingException, UnsupportedEncodingException {
 
@@ -33,26 +49,39 @@ public class MailAuthService {
         mailSender.sendEmail(emailAddress,message);
         return codeDto.getCodeId();
     }
+    public CodeUserDto validateCodeForPassword(Integer codeId, String code) {
+        return codeValidator.validateCodeForPassword(codeId, code);
+    }
+
+
+
+
+
+
+
+    public void sendPassword(String emailAddress, String newPassword) throws MessagingException, UnsupportedEncodingException {
+        String message = "초기화된 비밀번호 입니다   " + newPassword;
+        mailSender.sendEmail(emailAddress,message);
+    }
 
     public void sendUsernames(String emailAddress, String usernames) throws MessagingException, UnsupportedEncodingException {
         String message = "해당 이메일로 가입된 아이디 목록입니다   " + usernames;
         mailSender.sendEmail(emailAddress,message);
     }
 
-    public void validateCode(Integer codeId, String code) {
-        codeValidator.validateCode(codeId, code);
+    public Integer sendCodeForEmail(String email) throws MessagingException, UnsupportedEncodingException {
 
-    }
-    public CodeUserDto validateCodeForPassword(Integer codeId, String code) {
-        return codeValidator.validateCodeForPassword(codeId, code);
-    }
-
-    public void validateAuthenticatedEmail(Integer codeId,String email){
-        codeValidator.validateAuthenticatedEmail(codeId,email);
+        CodeDto codeDto = codeValidator.generateCodeForEmailUpdate(email);
+        String message = "인증코드를 입력해주세요 " + codeDto.getCode();
+        mailSender.sendEmail(email,message);
+        return codeDto.getCodeId();
     }
 
-    public void sendPassword(String emailAddress, String newPassword) throws MessagingException, UnsupportedEncodingException {
-        String message = "초기화된 비밀번호 입니다   " + newPassword;
-        mailSender.sendEmail(emailAddress,message);
+    public String validateCodeForEmail(Integer codeId, String code) {
+        return codeValidator.validateCodeForEmailUpdate(codeId, code);
+    }
+
+    public String validateCodeForJoinAgain(Integer codeId, String code, String email) {
+        return codeValidator.validateCodeForJoinAgain(codeId,code,email);
     }
 }
