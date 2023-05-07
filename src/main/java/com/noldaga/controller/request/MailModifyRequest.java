af779575e19e85d231e6401ea346bc0a7af851e5
jiwan <jiwan5605@naver.com>
2023-05-07 21:51:02 +0900

    그룹관리자->그룹원강퇴기능 + 그룹 view 멤버추가

---
+++ src/main/java/com/noldaga/controller/request/MailModifyRequest.java
@@ -0,0 +1,23 @@
+package com.noldaga.controller.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@NoArgsConstructor
+public class MailModifyRequest {
+
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+
+    @Positive
+    private Integer codeId;
+}
