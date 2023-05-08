de048a3bea3b509990a7a5fba64f428a1780961d
areumh01 <areumh01@gmail.com>
2023-05-08 10:15:11 +0900

    공유 버튼 삭제

--- src/main/resources/static/assets/js/Feed/main-page.js
+++ src/main/resources/static/assets/js/Feed/main-page.js
@@ -228,56 +228,6 @@ function feedContent(data) {
                     <a class="nav-link save" href="#!" onclick="save(${data.id})">
                       <i class="bi bi-bookmark-check-fill pe-1"></i>저장하기</a>
                   </li>
-                  <!-- Card share action START -->
-                  <li class="nav-item dropdown ms-sm-auto">
-                    <a
-                      class="nav-link mb-0"
-                      href="#"
-                      id="cardShareAction"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <i class="bi bi-reply-fill flip-horizontal ps-1"></i>공유하기(##)
-                    </a>
-                    <!-- Card share action dropdown menu -->
-                    <ul
-                      class="dropdown-menu dropdown-menu-end"
-                      aria-labelledby="cardShareAction"
-                    >
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          <i class="bi bi-envelope fa-fw pe-2"></i>Send via
-                          Direct Message</a
-                        >
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          <i class="bi bi-bookmark-check fa-fw pe-2"></i
-                          >Bookmark
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          <i class="bi bi-link fa-fw pe-2"></i>Copy link to
-                          post</a
-                        >
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          <i class="bi bi-share fa-fw pe-2"></i>Share post via
-                          …</a
-                        >
-                      </li>
-                      <li><hr class="dropdown-divider" /></li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          <i class="bi bi-pencil-square fa-fw pe-2"></i>Share to
-                          News Feed</a
-                        >
-                      </li>
-                    </ul>
-                  </li>
-                  <!-- Card share action END -->
                 </ul>
                 <!-- Feed react END -->
 
