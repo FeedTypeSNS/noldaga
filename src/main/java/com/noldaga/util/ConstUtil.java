e9a14bc5aab1368ef9e7e235f5260d608907bc01
jiwan <jiwan5605@naver.com>
2023-05-13 10:39:55 +0900

    group default img 추가

--- src/main/java/com/noldaga/util/ConstUtil.java
+++ src/main/java/com/noldaga/util/ConstUtil.java
@@ -3,4 +3,6 @@ package com.noldaga.util;
 public class ConstUtil {
 
     public static final String USER_DEFAULT_IMG_URL = "https://kr.object.ncloudstorage.com/noldaga-s3/noldaga-nonImg.png";
+    public static final String GROUP_DEFAULT_IMG_URL = "https://kr.object.ncloudstorage.com/noldaga-s3/noldaga-nonImg.png";
+
 }
